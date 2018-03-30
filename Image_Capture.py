e59e34591e338af67776cd7c64299abcae0308af
Suruchi Chopra <suransh2008@gmail.com>
2018-03-30 20:42:43 +0530

    Added Requirements

--- Image_Capture.py
+++ Image_Capture.py
@@ -6,7 +6,6 @@
 
 import numpy as np
 import cv2
-import matplotlib.pyplot as plt
 import os
 
 
---
+++ RequirementsPy2.sh
@@ -0,0 +1,4 @@
+sudo pip install numpy
+sudo pip install scipy
+sudo pip install scikit-learn
+sudo pip install keras
